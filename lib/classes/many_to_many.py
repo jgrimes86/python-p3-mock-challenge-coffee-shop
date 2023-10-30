49d60f61b8947ca461a0131fab67b718579bba17
Jim Grimes <jimgrimes86@gmail.com>
2023-10-30 10:26:31 -0400

    Completed Initializers & Properties, Obj Relationshp Methods & Properties

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,13 +1,34 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
+
+    def __repr__(self):
+        return f"<Coffee: {self.name}>"
+    
+    def get_name(self):
+        return self._name
+
+    def set_name(self, new_name):
+        if not hasattr(self, '_name'):
+            if (type(new_name) == str) and (3 <= len(new_name)):
+                self._name = new_name
+            else:
+                raise Exception("Coffee name must be a string with at least 3 characters.")
+        else:
+            raise Exception("Cannot rename coffee")
+
+    name = property(get_name, set_name)
+
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
-    
+        unique_customer_list = []
+        for order in self.orders():
+            if order.customer not in unique_customer_list:
+                unique_customer_list.append(order.customer)
+        return unique_customer_list
+
     def num_orders(self):
         pass
     
@@ -18,17 +39,78 @@ class Customer:
     def __init__(self, name):
         self.name = name
         
+    def __repr__(self):
+        return f"<Customer: {self.name}>"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if (type(new_name) == str) and (1 <= len(new_name) <= 15):
+            self._name = new_name
+        else:
+            raise Exception("Name must be a string between 1 and 15 characters long.")
+
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        unique_coffee_list = []
+        for order in self.orders():
+            if order.coffee not in unique_coffee_list:
+                unique_coffee_list.append(order.coffee)
+        return unique_coffee_list
     
     def create_order(self, coffee, price):
         pass
     
 class Order:
+
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    def __repr__(self):
+        return f"<Order of {self.coffee.name} for {self.customer.name}>"
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if not hasattr(self, '_price'):
+            if type(new_price) == float and (1.0 <= new_price <= 10.0):
+                self._price = new_price
+            else:
+                raise Exception("Price must be a float between 1.0 and 10.0, inclusive.")
+        else:
+            raise Exception("Cannot change price.")
+    
+    @property
+    def customer(self):
+        return self._customer
+    
+    @customer.setter
+    def customer(self, new_customer):
+        if isinstance(new_customer, Customer):
+            self._customer = new_customer
+        else:
+            raise Exception("Order must include a Customer instance.")
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, new_coffee):
+        if isinstance(new_coffee, Coffee):
+            self._coffee = new_coffee
+        else:
+            raise Exception("Order must include a Coffee instance.")
--- lib/debug.py
+++ lib/debug.py
@@ -5,6 +5,18 @@ from classes.many_to_many import Customer
 from classes.many_to_many import Order
 from classes.many_to_many import Coffee
 
+
+cust1 = Customer("Stephen")
+cust2 = Customer("Amy")
+
+cof1 = Coffee("Latte")
+cof2 = Coffee("Espresso")
+
+ord1 = Order(cust1, cof1, 5.25)
+ord2 = Order(cust2, cof1, 5.20)
+ord3 = Order(cust1, cof2, 4.75)
+ord4 = Order(cust1, cof1, 5.25)
+
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
